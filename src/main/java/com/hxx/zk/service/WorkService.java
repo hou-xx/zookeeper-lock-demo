064a3dd11094215be71e207e142e97c5f39aed94
houxx <hou_xiangxiang@126.com>
2019-09-19 17:22:06 +0800

    添加调用实现

---
+++ src/main/java/com/hxx/zk/service/WorkService.java
@@ -0,0 +1,51 @@
+package com.hxx.zk.service;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+
+/**
+ * <ul>
+ * <li>功能说明：模拟业务</li>
+ * <li>作者：tal on 2019/9/19 0019 16:19 </li>
+ * <li>邮箱：houxiangxiang@cibfintech.com</li>
+ * </ul>
+ */
+public class WorkService {
+
+    private static String connectUrl = "10.21.41.181:2181,10.21.42.47:2181,10.21.49.252:2181";
+
+    public void work(Long time) {
+        //创建zookeeper的客户端
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient(connectUrl, retryPolicy);
+        client.start();
+        //创建分布式锁, 锁空间的根节点路径为/curator-test/lock
+        InterProcessMutex mutex = new InterProcessMutex(client, "/curator-test/lock");
+        try {
+            mutex.acquire();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("尝试获取锁发生异常！");
+            return;
+        }
+        //获得了锁, 进行业务流程
+        System.out.println(Thread.currentThread().getName() + " --> Enter mutex");
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        //完成业务流程, 释放锁
+        try {
+            mutex.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("尝试获取锁发生异常！");
+        }
+        //关闭客户端
+        client.close();
+    }
+}
---
+++ src/main/java/com/hxx/zk/starter/Demo.java
@@ -0,0 +1,28 @@
+package com.hxx.zk.starter;
+
+import com.hxx.zk.service.WorkService;
+
+/**
+ * <ul>
+ * <li>功能说明：启动测试</li>
+ * <li>作者：tal on 2019/9/19 0019 16:09 </li>
+ * <li>邮箱：houxiangxiang@cibfintech.com</li>
+ * </ul>
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+
+        final WorkService workService = new WorkService();
+        int flag = 0;
+        while (flag < 10) {
+            //模拟业务操作
+            new Thread(new Runnable() {
+                public void run() {
+                    workService.work(5000L);
+                }
+            }).start();
+            flag++;
+        }
+    }
+}
